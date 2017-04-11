987ad13e47476fd556e44df1a7288eee4ef9d3e9
Henry Wu <Henry Wu>
2017-04-10 20:58:26 -0700

    Finished

--- scripts.js
+++ scripts.js
@@ -1,8 +1,27 @@
-function submitForm() {
-	document.getElementById('email-form').submit();
-	document.getElementById('email-field').value = '';
-	document.getElementById('email-field').style.pointerEvents = 'none';
-	document.getElementById('email-label').style.textAlign = 'center';
-	document.getElementById('email-label').innerHTML = 'Thank yous! You will now receive updates for releases, show dates, etc. for 13th Zodiac!';
-	document.getElementById('submit-email').style.visibility = 'hidden';
-}
+window.onload = function () {
+	function submitForm () {
+		document.getElementById('email-form').submit();
+		document.getElementById('email-field').value = '';
+		document.getElementById('email-field').style.pointerEvents = 'none';
+		document.getElementById('email-label').style.textAlign = 'center';
+		document.getElementById('email-label').innerHTML = 'Thank you!';
+		document.getElementById('submit-email').style.visibility = 'hidden';
+	}
+
+	var modal = document.getElementsByClassName('modal')[0];
+	var snapchat = document.getElementById('snapchat-modal');
+	var close = document.getElementsByClassName('close-modal')[0];
+
+	snapchat.onclick = function() {
+		modal.style.display="block";
+	}
+
+	close.onclick = function() {
+		modal.style.display="none";
+	}
+	window.onclick = function(e) {
+		if (e.target == modal) {
+			modal.style.display = "none";
+		}
+	}
+};
